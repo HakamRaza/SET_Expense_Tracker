ca30790ba8942da9c8a3f31427605f6e0587a0f2
HakamRaza <ismatcd@yahoo.com>
2020-08-17 15:13:38 +0800

    style main page, add link to Learn More

--- Web/web_expense_tracker/src/containers/main/index.js
+++ Web/web_expense_tracker/src/containers/main/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import './main.css';
+import {Link} from 'react-router-dom';
 
 
 
 class Main extends React.Component{
     render(){
         return(
-            <div>
-                <h1>This is Main Web Page</h1>
+            <div className="main">
+                <div className="main-card">
+                    <div className="main-text">
+                        <h2>Expenses Tracking Made Easy</h2>
+                        <p>get an in depth analysis of your financial transaction in one place.</p>
+                        <Link to={"/register"}><button className="main-button">Learn More</button></Link>
+                    </div>
+                </div>
             </div>
         );
     }
