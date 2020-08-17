105fd027f51e542b3b53e58bc3d3d1cf9e267aba
HakamRaza <ismatcd@yahoo.com>
2020-08-17 16:05:20 +0800

    add icon main page

--- Web/web_expense_tracker/src/containers/main/index.js
+++ Web/web_expense_tracker/src/containers/main/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './main.css';
 import {Link} from 'react-router-dom';
-
+import { FaInstagram, FaLinkedin, FaFacebookSquare } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 
 class Main extends React.Component{
@@ -13,6 +14,20 @@ class Main extends React.Component{
                         <h2>Expenses Tracking Made Easy</h2>
                         <p>get an in depth analysis of your financial transaction in one place.</p>
                         <Link to={"/register"}><button className="main-button">Learn More</button></Link>
+                    </div>
+
+                    <div className="main-icons-holder">
+                        <IconContext.Provider value={{ className: 'main-icons' }}>
+                            <FaInstagram />
+                        </IconContext.Provider>
+
+                        <IconContext.Provider value={{ className: 'main-icons' }}>
+                            <FaLinkedin />
+                        </IconContext.Provider>
+
+                        <IconContext.Provider value={{ className: 'main-icons' }}>
+                            <FaFacebookSquare />
+                        </IconContext.Provider>
                     </div>
                 </div>
             </div>
