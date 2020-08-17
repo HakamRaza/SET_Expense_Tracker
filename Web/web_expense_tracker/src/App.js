8631c1b40d6116e998e1538bdfd64a70ce1a1d5b
HakamRaza <ismatcd@yahoo.com>
2020-08-17 14:07:13 +0800

    route login, register, main page to App.js

--- Web/web_expense_tracker/src/App.js
+++ Web/web_expense_tracker/src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import Header from 'components/header';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
+import Main from 'containers/main';
+import Login from 'containers/auth/Login';
+import Register from 'containers/auth/Register';
+
 function App() {
   return (
     <div>
       <Router>
         <Header/>
         <Switch>
-          
-          <Route />
-          <Route />
-          <Route />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/" component={Main} />
           <Route />
           <Route />
 
