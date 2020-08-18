24cbf07f0e4c9702e20d4abfd962d7b28dc3aee0
HakamRaza <ismatcd@yahoo.com>
2020-08-18 14:07:21 +0800

    complete main page with mobile responsive

--- Web/web_expense_tracker/src/containers/main/index.js
+++ Web/web_expense_tracker/src/containers/main/index.js
@@ -1,37 +1,39 @@
 import React from 'react';
-import './main.css';
+import { FaInstagram, FaTwitterSquare, FaFacebookSquare } from "react-icons/fa";
 import {Link} from 'react-router-dom';
-import { FaInstagram, FaLinkedin, FaFacebookSquare } from "react-icons/fa";
 import { IconContext } from "react-icons";
-
+import './main.css';
 
 class Main extends React.Component{
     render(){
         return(
             <div className="main">
+
                 <div className="main-card">
                     <div className="main-text">
                         <h2>Expenses Tracking Made Easy</h2>
                         <p>get an in depth analysis of your financial transaction in one place.</p>
-                        <Link to={"/register"}><button className="main-button">Learn More</button></Link>
+                        <Link to={"/learn-more"}><button className="main-button">Learn More</button></Link>
                     </div>
+
+                    <div className="main-bgp"></div>
 
                     <div className="main-icons-holder">
                         <IconContext.Provider value={{ className: 'main-icons' }}>
-                            <FaInstagram />
+                            <a href="https://www.instagram.com/" title="Instagram Official"><FaInstagram /></a>
                         </IconContext.Provider>
 
                         <IconContext.Provider value={{ className: 'main-icons' }}>
-                            <FaLinkedin />
+                            <a href="https://twitter.com/" title="Twitter Official"><FaTwitterSquare /></a>
                         </IconContext.Provider>
 
                         <IconContext.Provider value={{ className: 'main-icons' }}>
-                            <FaFacebookSquare />
+                            <a href="https://www.facebook.com/" title="Facebook Official"><FaFacebookSquare /></a>
                         </IconContext.Provider>
                     </div>
                 </div>
             </div>
-        );
+        )
     }
 }
 
