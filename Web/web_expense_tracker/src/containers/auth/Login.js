02aabdcdf2cc07287a4c6fc6c0d881d9b35d2cc1
HakamRaza <ismatcd@yahoo.com>
2020-08-18 17:55:26 +0800

    finish login mobile

--- Web/web_expense_tracker/src/containers/auth/Login.js
+++ Web/web_expense_tracker/src/containers/auth/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './auth.css';
-
+import { IoIosMail, IoMdKey} from "react-icons/io";
+import { IconContext } from "react-icons";
 
 class Login extends React.Component{
     constructor(){
@@ -17,16 +18,24 @@ class Login extends React.Component{
             <div className="auth">
                 <div className="auth-card">
                     <p>Login Into Your Dashboard</p>
-                    <div className="auth-bgp"></div>
-                    <div className="auth-form">
-                        <label htmlFor="email">Email :</label>
-                        <input type = "text" placeholder ="Email" id="email" onChange={(email)=> this.setState({email: email.target.value})}></input>
-                        
-                        <label htmlFor="pass">Password :</label>
-                        <input type = "password" placeholder ="Password" id="pass" onChange = {(password) => this.setState({password: password.target.value})}></input>
 
-                        <button>Submit</button>
+                    <div className="auth-bgp">
+                        <div>
+                            <div className="auth-form">
+                                <label htmlFor="email">Email :</label>
+                                <IconContext.Provider value={{ className: 'auth-icons' }}><IoIosMail /></IconContext.Provider>
+                                <input type = "text" placeholder ="Email" id="email" onChange={(email)=> this.setState({email: email.target.value})}></input>
+                            </div>
+
+                            <div className="auth-form">
+                                <label htmlFor="pass">Password :</label>
+                                <IconContext.Provider value={{ className: 'auth-icons' }}><IoMdKey /></IconContext.Provider>
+                                <input type = "password" placeholder ="Password" id="pass" onChange = {(password) => this.setState({password: password.target.value})}></input>
+                            </div>
+                        </div>
+                        <button className="auth-button">Submit</button>
                     </div>
+
                 </div>
         </div>
         );
