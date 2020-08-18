8ca9358b67ba142c7b94a715f1b9f01c090ab217
HakamRaza <ismatcd@yahoo.com>
2020-08-18 16:56:53 +0800

    add basic layout login

--- Web/web_expense_tracker/src/containers/auth/Login.js
+++ Web/web_expense_tracker/src/containers/auth/Login.js
@@ -1,13 +1,34 @@
 import React from 'react';
-
+import './auth.css';
 
 
 class Login extends React.Component{
+    constructor(){
+        super();
+
+        this.state={
+            email:"",
+            password:"",
+        }
+    }
+
     render(){
         return(
-            <div>
-                <h1>This is Login Page</h1>
-            </div>
+            <div className="auth">
+                <div className="auth-card">
+                    <p>Login Into Your Dashboard</p>
+                    <div className="auth-bgp"></div>
+                    <div className="auth-form">
+                        <label htmlFor="email">Email :</label>
+                        <input type = "text" placeholder ="Email" id="email" onChange={(email)=> this.setState({email: email.target.value})}></input>
+                        
+                        <label htmlFor="pass">Password :</label>
+                        <input type = "password" placeholder ="Password" id="pass" onChange = {(password) => this.setState({password: password.target.value})}></input>
+
+                        <button>Submit</button>
+                    </div>
+                </div>
+        </div>
         );
     }
 }
