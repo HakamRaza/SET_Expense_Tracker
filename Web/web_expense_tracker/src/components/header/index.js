c5a72a73693854f71e9dfcc98c28b0ae3dfdbad9
HakamRaza <ismatcd@yahoo.com>
2020-08-18 14:32:19 +0800

    add backdrop on touch to link header

--- Web/web_expense_tracker/src/components/header/index.js
+++ Web/web_expense_tracker/src/components/header/index.js
@@ -7,11 +7,12 @@ export default class Header extends React.Component {
             <div className="header">
                 <div className="header-logo"></div>
                 <ul className="header-list">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Product</li>
-                    <li>Contacts</li>
+                    <li><u>Home</u></li>
+                    <li><u>About</u></li>
+                    <li><u>Products</u></li>
+                    <li><u>Contacts</u></li>
                 </ul>
+            
                 <div>
                     <button className="header-register">Register </button>
                     <button className="header-login">Login </button>
