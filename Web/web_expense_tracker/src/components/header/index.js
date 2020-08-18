22bfa2d3d72b450acfe5186dec4b73239aa80898
HakamRaza <ismatcd@yahoo.com>
2020-08-18 16:05:13 +0800

    add mobile responsive main header

--- Web/web_expense_tracker/src/components/header/index.js
+++ Web/web_expense_tracker/src/components/header/index.js
@@ -1,10 +1,29 @@
 import React from "react";
 import './header.css';
+import { FaAlignJustify } from "react-icons/fa";
+import { IconContext } from "react-icons";
+
 
 export default class Header extends React.Component {
+    
+
     render(){
         return(
             <div className="header">
+
+                <div className="header-mobile" tabIndex="1">
+                    <IconContext.Provider value={{ className: 'header-mobile-icons' }}>
+                        <FaAlignJustify/>
+                    </IconContext.Provider>
+
+                    <ul className="header-list-mobile">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Products</li>
+                        <li>Contacts</li>
+                    </ul>
+                </div>
+
                 <div className="header-logo"></div>
                 <ul className="header-list">
                     <li><u>Home</u></li>
