627c020217338d286349a7fd09f9109b0ff99182
HakamRaza <ismatcd@yahoo.com>
2020-08-19 12:35:49 +0800

    create dash basic layout

--- Web/web_expense_tracker/src/components/drawer/index.js
+++ Web/web_expense_tracker/src/components/drawer/index.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import { FaAlignJustify, FaUserSlash} from "react-icons/fa";
 import { IconContext } from "react-icons";
+import './drawer.css';
 
 export default class Drawer extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            showDrawer: false,
+        }
+    }
+
+    _onLogout(){
+        console.log("logout user");
+    }
+
     render(){
         return(
-            <div>
-                <div>
-                    <IconContext.Provider value={{ className: 'drawer-icon'}}>
-                        <FaAlignJustify/>
-                    </IconContext.Provider>
+            <div className="drawer-cont">
 
-                    <div className="drawer-menu">
-                        <ul>
-                            <li>Settings</li>
-                            <li>Categories</li>
-                            <li>History</li>
-                            <li>FAQ</li>
-                            <li>HELP</li>
-                        </ul>
+                <div className="drawer-submenu">
+                    <div onClick={()=>{this.setState({showDrawer:!this.state.showDrawer})}}>
+                        <IconContext.Provider value={{ className: 'drawer-menu-icon'}}>
+                            <FaAlignJustify/>
+                        </IconContext.Provider>
                     </div>
+
+
+                    {this.state.showDrawer && (
+                        <div className="drawer-menu">
+                            <ul>
+                                <li>Settings</li>
+                                <li>Categories</li>
+                                <li>History</li>
+                                <li>FAQ</li>
+                                <li>HELP</li>
+                            </ul>
+                        </div>
+                    )}
                 </div>
-                <div>
-                    <IconContext.Provider value={{ className: 'drawer-icon'}}>
+
+                <div className="drawer-logout" onClick={()=>this._onLogout()}>
+                    <IconContext.Provider value={{ className: 'drawer-icon-logout'}}>
                         <FaUserSlash/>
                     </IconContext.Provider>
                     <p>Logout</p>
--- Web/web_expense_tracker/src/containers/dashboard/index.js
+++ Web/web_expense_tracker/src/containers/dashboard/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Drawer from '../../components/drawer';
+import './dashboard.css';
 
 
 export default class Dashboard extends React.Component{
     render(){
         return(
-            <div>
+            <div className="dash-container">
                 <Drawer />
-                <h1>This is Dashboard</h1>
+                <div className="dash-lngr">
+                    <p>Graph here</p>
+                </div>
+                <div className="dash-sum">
+                    <p>Summary here</p>
+                </div>
+                <div className="dash-categ">
+                    <p>Categories summary here</p>
+
+                </div>
+                <div className="dash-latest-trans">
+                    <p>Latest transaction here</p>
+
+                </div>
             </div>
         );
     }
