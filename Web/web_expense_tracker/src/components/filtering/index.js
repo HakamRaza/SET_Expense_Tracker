3b16c2148fb5d6df3485ccc0aca7e3f9e3246525
HakamRaza <ismatcd@yahoo.com>
2020-08-24 04:58:00 +0800

    repair search page

--- Web/web_expense_tracker/src/components/filtering/index.js
+++ Web/web_expense_tracker/src/components/filtering/index.js
@@ -1,12 +1,77 @@
 import React from 'react';
 import './filtering.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card'
 
 export default class FilterBar extends React.Component{
     render(){
         return(
             <div>
                 <p>Search Parameter: </p>
-                <div className='filter-container'>
+
+                <div className="filter-cont">
+                <Accordion>
+                    <Card bg='light'>
+                        <Accordion.Toggle as={Card.Header} eventKey="0" style={{textAlign:"center"}}>
+                        Click Here Start ..
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                                <Form className="filter-holder">
+
+                                    <Form.Group>
+                                        <Form.Label>Select Start Date</Form.Label>
+                                        <Form.Control size="sm" type="text" placeholder="Select Start Date ..." />
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.Label>Select End Date</Form.Label>
+                                        <Form.Control size="sm" type="text" placeholder="Select End Date ..." />
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.Label>Range Starting Price</Form.Label>
+                                        <Form.Control size="sm" type="text" placeholder="Starting Price ..." />
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.Label>Range Ending Price :</Form.Label>
+                                        <Form.Control size="sm" type="text" placeholder="Ending Price ..." />
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.Label>Description :</Form.Label>
+                                        <Form.Control size="sm" type="text" placeholder="Description Unique Identifier ..." />
+                                    </Form.Group>
+
+                                    <Form.Group controlId="exampleForm.ControlSelect1">
+                                        <Form.Label>Category</Form.Label>
+                                        <Form.Control size="sm" as="select">
+                                        <option>Category 1</option>
+                                        <option>Category 2</option>
+                                        <option>Category 3</option>
+                                        <option>Category 4</option>
+                                        <option>Category 5</option>
+                                        </Form.Control>
+                                    </Form.Group>
+
+                                </Form>
+                                    <Button variant="primary" type="submit">Search</Button>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
+
+
+
+                </div>
+
+
+
+
+                {/* <div className='filter-container'>
                     <div className="filter-holder">
                         <div className="filter-form">Categories Select</div>
                         <div className="filter-form">Start Date Select</div>
@@ -18,7 +83,7 @@ export default class FilterBar extends React.Component{
                     <div className="filbut-holder">
                         <button className="filter-button">Search</button>
                     </div>
-                </div>
+                </div> */}
             </div>
         );
     }
--- Web/web_expense_tracker/src/containers/transactions/index.js
+++ Web/web_expense_tracker/src/containers/transactions/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import Drawer from '../../components/drawer';
-import Sidebar from '../../components/sidebar';
+// import Sidebar from '../../components/sidebar';
 import FilterBar from '../../components/filtering';
-import TransactionCard from '../../components/transactionCard';
+import Table from 'react-bootstrap/Table';
+import './transaction.css';
+import { IconContext } from "react-icons";
+import { IoIosTrash, IoMdCreate } from "react-icons/io";
+
+
+
+
 
 const sumTrans = [
     {
@@ -88,16 +95,59 @@ const sumTrans = [
 export default class Transactions extends React.Component{
     render(){
         return(
-            <div>
+            <div className="latest-trans">
                 <Drawer />
-                {/* <Sidebar/> */}
-                <p>This is Transaction Page</p>
                 <FilterBar/>
-                <p><b>Latest Transactions :</b></p>
-                <div className="dash-latest-trans">
-                    {sumTrans.map((item)=>(
-                        <TransactionCard date={item.date} categ={item.categories} desc={item.desc} total={item.total.toFixed(2)}/>
-                    ))}
+                <p><b>Results :</b></p>
+                <br/>
+                <div>
+                    <Table responsive striped bordered hover size="sm">
+                        <thead>
+                            <tr>
+                            <th>Total Results</th>
+                            <th>Total Expenses</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className="trans-col">000 Results</td>
+                                <td className="trans-col">$ 0000.00</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+
+                <div style={{maxHeight:400, overflowY:"scroll"}}>
+                    <Table responsive striped bordered hover size="sm">
+                        <thead>
+                            <tr>
+                            <th>Date</th>
+                            <th>Categories</th>
+                            <th>Total</th>
+                            <th className="trans-col-desc">Description</th>
+                            <th>Del/ Edit</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {sumTrans.map((item)=>(
+                                <tr>
+                                    <td className="trans-col">{item.date}</td>
+                                    <td className="trans-col">{item.categories}</td>
+                                    <td className="trans-col">{item.total.toFixed(2)}</td>
+                                    <td className="trans-col-desc">{item.desc}</td>
+                                    <td className="trans-col">
+                                        <div>
+                                        <IconContext.Provider value={{ className: 'trans-icons' }}>
+                                            <IoIosTrash/>
+                                            <IoMdCreate/>
+                                        </IconContext.Provider>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+
                 </div>
             </div>
         );
