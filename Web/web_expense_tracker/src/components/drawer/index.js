015b18008dd5fce69824296dcb83f8d748e17aa6
HakamRaza <ismatcd@yahoo.com>
2020-08-23 23:21:45 +0800

    repair user header

--- Web/web_expense_tracker/src/components/drawer/index.js
+++ Web/web_expense_tracker/src/components/drawer/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { FaAlignJustify, FaUserSlash} from "react-icons/fa";
-import { IconContext } from "react-icons";
 import './drawer.css';
-import {Link} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 
 export default class Drawer extends React.Component{
     constructor(props){
@@ -21,35 +24,31 @@ export default class Drawer extends React.Component{
     render(){
         return(
             <div className="drawer-cont">
-
-                <div className="drawer-submenu">
-                    <div onClick={()=>{this.setState({showDrawer:!this.state.showDrawer})}}>
-                        <IconContext.Provider value={{ className: 'drawer-menu-icon'}}>
-                            <FaAlignJustify/>
-                        </IconContext.Provider>
-                    </div>
-
-
-                    {this.state.showDrawer && (
-                        <div className="drawer-menu">
-                            <ul>
-                                <li>Settings</li>
-                                <li>Categories</li>
-                                <li>History</li>
-                                <li>FAQ</li>
-                                <li>HELP</li>
-                            </ul>
+                    <Navbar bg="light" expand='sm'>
+                    <Navbar.Brand href="/dashboard">
+                        <div className="header-logo">
+                            <h2 style={{marginLeft:25, paddingTop:18}}>Okane</h2>
                         </div>
-                    )}
-                </div>
-                
-                <Link to={"/login"}>
-                <div className="drawer-logout" onClick={()=>this._onLogout()}>
-                    <IconContext.Provider value={{ className: 'drawer-icon-logout'}}>
-                        <FaUserSlash/>
-                    </IconContext.Provider>
-                    <p>Logout</p>
-                </div></Link>
+                    </Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto">
+                            <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+
+                            <NavDropdown title="Settings" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="/transaction">Transaction History</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="/category">Category Settings</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="/monthly-summary">Monthly Overview</NavDropdown.Item>
+                            </NavDropdown>
+                            </Nav>
+                            <Form inline>
+                                <Button variant="danger" type="submit" onClick={()=>(this._onLogout())}>Logout</Button>
+                            </Form>
+                        </Navbar.Collapse>
+                    </Navbar>
             </div>
 
         );
