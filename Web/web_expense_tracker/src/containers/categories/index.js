dde1907756892917ecf6b8f43ccd91c61eaa7b91
HakamRaza <ismatcd@yahoo.com>
2020-08-28 06:51:00 +0800

    get list of categories, category

--- Web/web_expense_tracker/src/containers/categories/index.js
+++ Web/web_expense_tracker/src/containers/categories/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import Drawer from '../../components/drawer';
-// import Sidebar from '../../components/sidebar';
 import './categories.css';
-import Modal from '../../components/modal';
-// import apiData from '../../apiData';
+import Actions from '../../actions';
+import { connect } from "react-redux";
+
+
+import Drawer from '../../components/drawer';
 // import CategoriesCard from '../../components/categoriesCard';
+
+// import { IconContext } from "react-icons";
+// import Modal from '../../components/modal';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
-import { IconContext } from "react-icons";
-import { IoIosTrash, IoMdCreate } from "react-icons/io";
+import { IoIosTrash, IoMdCreate, IoMdAlbums } from "react-icons/io";
 
 
 const sumCat = [
@@ -92,87 +98,275 @@ const sumCat = [
 ]
 
 
-export default class Category extends React.Component{
+class Category extends React.Component{
     constructor(){
         super();
 
         this.state={
-            showAddMore: false,
-            showDelCat: false,
-            showEditCat: false,
+            itemID:"",
+            category_title:"",
+            category_budget:"",
+            category_color:"",
+
+            currentMonth:"",
+            currentYear:"",
+
+            showModalDelete:false,
+            showModalUpdate:false,
+            showModalAdd:false,
+
+            barsData:[],
         }
     }
 
-    _addCategories(){
-        console.log("Add more Categories");
-        this.setState({showAddMore:true});
-        
+    componentDidMount(){
+        const d = new Date();
+        const yr = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+        const mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d);
+        // const dt = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+        // const today = `${yr}-${mo}-${dt}`;
+
+        this.setState({
+            currentMonth: mo,
+            currentYear: yr,
+            // currentDay: today,
+        }, () => {
+            
+            //callback function
+            this._onGetCategoriesBar();
+            // this._onGetMonthlyOverview();
+            // this._onGetLatestTransaction();
+        });
+
     }
 
+    componentDidUpdate(prevProps){
+        
+        // console.log("this is component did update");
+        
+        const { getBarsData} = this.props;
+        
+        if(prevProps.getBarsData.isLoading && !getBarsData.isLoading){
+            
+            if(getBarsData.data.status === "success") {
+                
+                this.setState({barsData: getBarsData.data.barsData});
+                
+                
+            } else if (getBarsData.error !== null){
+                
+                this.setState({
+                    showModalAlert:true,
+                    modalTitleAlert: "Failed",
+                    modalMsgAlert:"Failed to fetch Categories Bar List. Please Try Again",
+                });
+            }
+        }
+    }
+
+    _confirmation(selectID, id){
+        
+        const itemID = id;
+        this.setState({itemID:itemID});
+
+        switch (selectID) {
+            case "update":
+                this.setState({showModalUpdate:true});
+                // this.setState({showModalUpdate:true});
+
+            break;
+            
+            case "delete":
+                this.setState({showModalDelete:true});
+                // this.setState({showModalDelete:true,});
+            break;
+
+            case "add":
+                this.setState({showModalAdd:true});
+                // this.setState({showModalDelete:true,});
+            break;
+            
+            default:
+                break;
+        }
+    }
+
+    
     _delCategories(){
         console.log("Delete a Category");
-        this.setState({showDelCat:true});
-        
-    }
-
-    _editCategories(){
-        console.log("Edit a Category");
-        this.setState({showEditCat:true});
+        console.log(this.state.itemID);
+        // this.setState({showDelCat:true});
         
     }
     
-    render(){
-        // console.log(apiData[0].GetCategoryBar.barsData);
+    _editCategories(){
+        console.log("Edit a Category");
+        console.log(this.state.itemID);
+        // this.setState({showEditCat:true});
+    }
+    
+    _addCategories(){
+        console.log("Add more Categories");
+        // this.setState({showAddMore:true});
+    }
 
+    _onGetCategoriesBar(){
+        const { currentMonth, currentYear} = this.state;
+
+        const formData = {
+            month: currentMonth,
+            year : currentYear,
+        }
+
+        this.props.onGetBarsData(formData);
+    }
+
+    render(){
         return(
             <div>
                 <Drawer/>
-                <div className="cat-cont">
-                    
-                    <div className="cat-card-cont">
+                {this.state.showModalDelete && (
+                    <div>
+                    <Modal centered show={true} onHide={()=>this.setState({showModalDelete:false})}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Delete Category :</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>Delete this Category? <i>(This will affect your total expenses and saving calculation)</i></Modal.Body>
+
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={()=>this.setState({showModalDelete:false})}>Close</Button>
+                            <Button variant="danger" onClick={()=>this._delCategories()}>Delete</Button>
+                        </Modal.Footer>
+                    </Modal>
+                </div>)}
+
+                {this.state.showModalUpdate && (
+                    <div>
+                    <Modal centered show={true} onHide={()=>this.setState({showModalUpdate:false})}>
+                        <Modal.Header closeButton>
+                        <Modal.Title>Update Category :</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form.Group controlId="updateTitle">
+                                <Form.Label>Description :</Form.Label>
+                                <Form.Control required size="sm" type="text" pattern=".{1,10}" placeholder="Max 10 Chars"/>
+                            </Form.Group>
+
+                            <Form.Group controlId="updateBudget">
+                                <Form.Label>Budget (RM) :</Form.Label>
+                                <Form.Control required size="sm" type="number" min="0" step="0.01" placeholder="Budget Value" />
+                            </Form.Group>
+
+                            <Form.Group controlId="updateColor">
+                            <Form.Label>Color Selection :</Form.Label>
+                            <Form.Control required size="sm" as="select">
+                                <option>Color</option>
+                                <option>Color</option>
+                                <option>Color</option>
+                                <option>Color</option>
+                                <option>Color</option>
+                                {/* {this.state.getCategories.map( item=>(
+                                    <option value={item.id}>{item.category_title}</option>
+                                ))} */}
+                            </Form.Control>
+                            </Form.Group>
+                        </Modal.Body>
+                        <Modal.Footer>
+
+                            <Button variant="secondary" onClick={()=>this.setState({showModalUpdate:false})}>Close</Button>
+                            <Button variant="success" onClick={()=>this._editCategories()}>Save</Button>
                         
+                        </Modal.Footer>
+                    </Modal>
+                </div>)}
+                {this.state.showModalAdd && (
+                    <div>
+                    <Modal centered show={true} onHide={()=>this.setState({showModalAdd:false})}>
+                        <Modal.Header closeButton>
+                        <Modal.Title>Add Category :</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form.Group controlId="updateTitle">
+                                <Form.Label>Description :</Form.Label>
+                                <Form.Control required size="sm" type="text" pattern=".{1,10}" placeholder="Max 10 Chars"/>
+                            </Form.Group>
+
+                            <Form.Group controlId="updateBudget">
+                                <Form.Label>Budget (RM) :</Form.Label>
+                                <Form.Control required size="sm" type="number" min="0" step="0.01" placeholder="Budget Value" />
+                            </Form.Group>
+
+                            <Form.Group controlId="updateColor">
+                            <Form.Label>Color Selection :</Form.Label>
+                            <Form.Control required size="sm" as="select">
+                                <option>Color</option>
+                                <option>Color</option>
+                                <option>Color</option>
+                                <option>Color</option>
+                                <option>Color</option>
+                                {/* {this.state.getCategories.map( item=>(
+                                    <option value={item.id}>{item.category_title}</option>
+                                ))} */}
+                            </Form.Control>
+                            </Form.Group>
+                        </Modal.Body>
+                        <Modal.Footer>
+
+                            <Button variant="secondary" onClick={()=>this.setState({showModalAdd:false})}>Close</Button>
+                            <Button variant="primary" onClick={()=>this._addCategories()}>Add</Button>
+                        
+                        </Modal.Footer>
+                    </Modal>
+                </div>)}
+                <div className="cat-cont">
+                    <div className="cat-card-cont">
+                        {this.state.barsData.map(item=>(
                         <div className="categ-card-cont">
-                            <Card border="primary" style={{ width: '100%', marginTop:10,}}>
-                                <Card.Body>
-                                    <div style={{display:'flex', flexDirection:'row', justifyContent:"space-between"}} onClick={()=>{this._addCategories()}}>
-                                        <Card.Title><b> + Click Here to Add More</b></Card.Title>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-                        </div>
-
-                        {/* {apiData[0].GetCategoryBar.barsData.map(item=>( */}
-                        {sumCat.map(item=>(
-
-
-                        <div className="categ-card-cont">
-                            <Card border="primary" style={{ width: '100%', marginTop:10, backgroundColor: `${item.color}`}}>
+                            <Card border="primary" style={{ width: '100%', marginTop:10, boxShadow:`5px 5px 0px ${item.color}`}}>
 
                                 <Card.Body>
                                     <div style={{display:'flex', flexDirection:'row', justifyContent:"space-between"}}>
-                                        <Card.Title><b>{item.name} </b></Card.Title>
+                                        <Card.Title><b>{item.title} </b></Card.Title>
                                     </div>
                                     <div>
-                                    <Card.Text>Total Budget: $ {item.budget.toFixed(2)}</Card.Text>
-                                    </div>
-                                    <div>
-                                        <IconContext.Provider value={{ className: 'trans-icons' }}>
-                                            <IoIosTrash onClick={()=>{this._delCategories()}}/>
-                                            <IoMdCreate onClick={()=>{this._editCategories()}}/>
-                                        </IconContext.Provider>
+                                        <Card.Text>Budget: <b>$ {item.budget.toFixed(2)}</b></Card.Text>
+                                        <Button variant="warning" size="sm" onClick={()=>this._confirmation("update", item.id)}><IoMdCreate/> Edit</Button>{' '}
+                                        <Button variant="danger" size="sm" onClick={()=>this._confirmation("delete", item.id)}><IoIosTrash />Delete</Button>{' '}
                                     </div>
                                 </Card.Body>
                             </Card>
                         </div>
                         ))}
 
+                        <div className="categ-card-cont">
+                            <Card border="primary" style={{ width: '100%', marginTop:10, minHeight:147}}>
+                                <Card.Body style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+                                    <Button variant="primary" size="md" onClick={()=>this._confirmation("add")}><IoMdAlbums /> Add More</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+
                     </div>
                 </div>
-
-                {this.state.showAddMore && (<Modal butText = 'ADD' onclick={()=>this.setState({showAddMore:false})}/>)}
-                {this.state.showEditCat && (<Modal butText = 'SAVE' name="Category A" onclick={()=>this.setState({showEditCat:false})}/>)}
-                {this.state.showDelCat && (<Modal butText = 'DELETE' name="Category B" onclick={()=>this.setState({showDelCat:false})}/>)}
             </div>
         );
     }
 }
+
+const mapStateToProps = store => ({
+    // deleteTransactionData: Actions.deleteTransactionData(store),
+    getBarsData: Actions.getBarsData(store),
+    //
+    // getOverviewData: Actions.getOverviewData(store),
+    // getTransactionData: Actions.getTransactionData(store),
+
+});
+
+const mapDispatchToProps = {
+    // onDeleteTransaction: Actions.delete_transaction,
+    onGetBarsData: Actions.get_bars,
+    // onGetOverviewData: Actions.get_overview,
+    // onGetTransaction: Actions.get_transaction,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Category);
